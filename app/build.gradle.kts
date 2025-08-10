69cb081c85cf79ff076d47e5794ddf51948727a5
Karlo M <karlo.marinovic@decode.agency>
2025-08-10 21:52:30 +0200

    chore: increase version before poublish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.imkaem.android.upuli"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
