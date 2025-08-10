d018d29241374a0207bd9a9b280b6a1f00c45c86
Karlo M <karlo.marinovic@decode.agency>
2025-08-10 18:39:51 +0200

    chore: update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.imkaem.android.upuli"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
